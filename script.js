d291c9c8c6d33c276af04d8184cd675af0aab322
Karthik R <karthikr.ofc@gmail.com>
2022-09-02 22:27:29 +0530

    Good Progress

---
+++ script.js
@@ -0,0 +1,9 @@
+var cvlink = document.getElementById('down');
+
+cvlink.addEventListener('click',change);
+
+function change()
+{
+    const ch = document.getElementById('down');
+    ch.textContent="Thanks!"
+}
